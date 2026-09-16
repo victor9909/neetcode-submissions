86ecf24ac04bded411947bd5c365e8164b7de93d
victor9909 <165707703+victor9909@users.noreply.github.com>
2026-09-16 19:12:28 +0200

    Add: decode-ways - submission-27

---
+++ Data Structures & Algorithms/decode-ways/submission-27.py
@@ -0,0 +1,29 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        
+        memo = {len(s): 1}
+
+        def backtrack(i):
+
+            if i in memo:
+                return memo[i]
+
+            if i >= len(s):
+                return 1
+            
+            res = 0
+            if(
+                s[i] in "123456789"
+            ):
+                res += backtrack(i + 1)
+            
+            if(
+                (s[i] in "1" and i + 1 < len(s) and s[i + 1] in "0123456789") or
+                (s[i] in "2" and i + 1 < len(s) and s[i + 1] in "0123456")
+            ):
+                res += backtrack(i + 2)
+            
+            memo[i] = res
+            return memo[i]
+        
+        return backtrack(0)
