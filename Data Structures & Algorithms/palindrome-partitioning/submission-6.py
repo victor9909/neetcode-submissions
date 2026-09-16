e71532711c604a7f72262c628fb99a21fdbdcada
victor9909 <165707703+victor9909@users.noreply.github.com>
2026-09-16 08:06:26 +0200

    Add: palindrome-partitioning - submission-6

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-6.py
@@ -0,0 +1,29 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        
+        def is_pal(l, r):
+
+            while l < r:
+                if s[l] != s[r]:
+                    return False
+                l += 1
+                r -= 1
+            return True
+        
+        res = []
+        curr = []
+        def backtrack(i):
+
+            if i >= len(s):
+                res.append(curr[::])
+                return
+            
+            for j in range(i, len(s)):
+                if is_pal(i, j):
+                    curr.append(s[i:j+1])
+                    backtrack(j + 1)
+                    curr.pop()
+        
+        backtrack(0)
+        print(res)
+        return res
