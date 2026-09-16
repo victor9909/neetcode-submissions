1281c6d643dd3ec0b4af3584f047df8eef000875
victor9909 <165707703+victor9909@users.noreply.github.com>
2026-09-16 18:20:35 +0200

    Add: house-robber - submission-37

---
+++ Data Structures & Algorithms/house-robber/submission-37.py
@@ -0,0 +1,30 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        
+        cache = {len(nums): 0}
+        
+        def backtrack(i):
+
+            if i in cache:
+                return cache[i]
+
+            if i >= len(nums):
+                return 0
+            
+            cache[i] = max(backtrack(i + 2), backtrack(i + 3)) + nums[i]
+            return cache[i]
+        
+        #return max(backtrack(0), backtrack(1))
+
+        prev2 = 0
+        prev1 = 0
+
+        for money in nums:
+            current = max(prev1, prev2 + money)
+            prev2 = prev1
+            prev1 = current
+
+        return prev1
+
+
+
