b5fce1862f4deb6da9dc66511be6e0609044da1c
victor9909 <165707703+victor9909@users.noreply.github.com>
2026-09-18 18:21:32 +0200

    Add: jump-game-ii - submission-12

---
+++ Data Structures & Algorithms/jump-game-ii/submission-12.py
@@ -0,0 +1,39 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        
+        memo = {}
+
+        def dfs(i):
+
+            if i in memo:
+                return memo[i]
+
+            if i >= len(nums) - 1:
+                return 0
+            
+            res = float("inf")
+            for j in range(nums[i]):
+                res = min(res, dfs(i + j + 1) + 1)
+            memo[i] = res
+            return res
+        
+        #return dfs(0)
+        """
+        dp = [float("inf")] * len(nums)
+        dp[-1] = 0
+
+        for i in range(len(nums) - 2, -1, -1):
+            for j in range(nums[i]):
+                dp[i] = min(dp[i], dp[i + j + 1] + 1) if i + j + 1 < len(nums) else 1
+        #return dp[0]
+        """
+        farthest, curr_end, jumps = 0, 0, 0
+
+        for i in range(len(nums) - 1):
+            farthest = max(farthest, i + nums[i])
+
+            if i == curr_end:
+                jumps += 1
+                curr_end = farthest
+
+        return jumps
