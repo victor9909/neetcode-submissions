e23c7a5529e79e5ebd485d06850f3dcd8729ce6b
victor9909 <165707703+victor9909@users.noreply.github.com>
2026-09-20 15:53:36 +0200

    Add: merge-intervals - submission-12

---
+++ Data Structures & Algorithms/merge-intervals/submission-12.py
@@ -0,0 +1,24 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        
+        
+        def is_overlap(int1, int2):
+            return int1[1] >= int2[0]
+        
+        intervals.sort()
+
+        res = []
+        prev_int = intervals[0]
+        res.append(prev_int)
+        for start, end in intervals[1:]:
+            if prev_int[1] >= start:
+                prev_int = [min(start, prev_int[0]), max(end, prev_int[1])]
+                res.pop()
+            else:
+                prev_int = [start, end]
+            
+            res.append(prev_int)
+        
+
+
+        return res
